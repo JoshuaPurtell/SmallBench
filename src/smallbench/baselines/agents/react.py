3cff533044a7b865e62dab24d6ac0bdb5f04a9bc
Josh Purtell <jmvpurtell@gmail.com>
2024-11-01 09:25:53 -0700

    update smallbench

--- src/smallbench/baselines/agents/react.py
+++ src/smallbench/baselines/agents/react.py
@@ -1,36 +1,71 @@
-# TODO: use a legit runtime, not python, for orchestration
-
-from typing import Any, Dict, List
-
-from pydantic import BaseModel
-
-from apropos import LLM
-from apropos.src.core.lms.cost import CostMonitor
+from pydantic import BaseModel, Field
+from typing import List, Dict, Any, Optional
+from zyk import LM
+from dataclasses import dataclass
 from smallbench.baselines.agents.core import Agent
+from apropos.src.core.lms.cost import CostMonitor
+from smallbench.benchmarks.bcb_a.abstractions import ActionResult
+import uuid
+from synth_sdk.tracing.decorators import trace_system
+
 
 REACT_LOOKBACK = 5
 
-class ReactResponse(BaseModel):
-    reasoning: str
-    action: str
-    action_args: Dict[str, Any]
+@dataclass
+class Step:
+    messages: List[Dict]
+    following_observation: Optional[ActionResult]
 
+    def to_dict(self) -> Dict[str, Any]:
+        if isinstance(self.following_observation, str):
+            self.following_observation = ActionResult(success=False, result=self.following_observation)
+        if not self.following_observation:
+            print("Warning: No observation")
+            self.following_observation = ActionResult(success=False, result="No observation")
+        return {
+            "messages": self.messages,
+            "observation": {
+                "success": self.following_observation.success,
+                "result": self.following_observation.result
+            }
+        }
+
+@dataclass
+class History:
+    steps: List[Step]
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {"steps": [step.to_dict() for step in self.steps]}
+
+class ActionArgument(BaseModel):
+    key: str
+    value: str
+
+class ReAct(BaseModel):
+    reasoning: str
+    action_name: str
+    action_args: List[ActionArgument]
 
 class SimpleReActLanguageAgent(Agent):
     obs_history: List[Dict]
     react_history: List[Dict]
-    lm: LLM
+    trajectory: List[Dict]
+    lm: LM
     contexts: List[Dict]
     cost_monitor: CostMonitor
+    history: History
     multi_threaded: bool = False
 
-    def __init__(self, lm: LLM, contexts: List[Dict], multi_threaded: bool = False):
+    def __init__(self, lm: LM, contexts: List[Dict], multi_threaded: bool = False):
         self.lm = lm
         self.cost_monitor = CostMonitor(model_name=lm.model_name)
         self.obs_history = []
         self.react_history = []
+        self.trajectory = []
         self.contexts = contexts
+        self.history = History(steps=[])
         self.multi_threaded = multi_threaded
+        self.agent_id = str(uuid.uuid4())  # Initialize agent_id here
 
     def load_context(self, contexts: List[Dict]):
         self.contexts = contexts
@@ -49,13 +84,12 @@ class SimpleReActLanguageAgent(Agent):
     def _prepare_messages(self):
         actions_snippet = ""
         for action_name, action_info in self.contexts["actions"].items():
-            action_info_snippet = "".join(
-                f"<{key}>\n{value}\n</{key}>\n" for key, value in action_info.items()
-            )
+            action_info_snippet = ""
+            for key, value in action_info.items():
+                action_info_snippet += f"<{key}>\n{value}\n</{key}>\n"
             actions_snippet += (
                 f"<{action_name}>\n{action_info_snippet}\n</{action_name}>\n"
             )
-
         system_message = f"""
 # Premise
 {self.contexts['premise']}
@@ -82,49 +116,77 @@ The environment one step in the past is your current environment.
 # Recent Observations
 {obs_history}
 
-Your response: """
+Your next actions / thought: """
         return system_message, user_message
 
     def _process_response(self, react_step, system_message, user_message):
+        self.history.steps.append(Step(messages=[{"role": "system", "content": system_message}, {"role": "user", "content": user_message}, {"role": "assistant", "content": str(react_step.dict())}], following_observation=None))
         self.cost_monitor.update_token_counts(
             system_message, user_message, str(react_step.dict())
         )
         self.react_history.append(react_step)
-        return react_step.action, react_step.action_args
+        arguments = [
+            {arg.key: arg.value} for arg in react_step.action_args
+        ]
+        return react_step.action_name, arguments
 
+    @trace_system(
+        origin="agent",
+        event_type="act",
+        manage_event="create",
+        increment_partition=True,
+        log_vars_input={},
+        log_vars_output={"react_step"},
+        verbose=True,
+    )
     async def act_async(self):
         system_message, user_message = self._prepare_messages()
         try:
-            react_step = await self.lm.async_respond(
-                system_prompt=system_message,
-                user_prompt=user_message,
-                response_model=ReactResponse,
+            react_step = await self.lm.respond_async(
+                system_message=system_message,
+                user_message=user_message,
+                response_model=ReAct,
             )
-            if "'await'" in react_step.reasoning:
-                raise Exception("ReAct agent found a coroutine error")
         except Exception as e:
             print(f"Error: {e}")
             raise e
+        self.trajectory.append(
+            {"messages": {
+                "system": system_message,
+                "user": user_message,
+                "assistant": str(react_step.dict())
+            }}
+        )
         return self._process_response(react_step, system_message, user_message)
 
+    @trace_system(
+        origin="agent",
+        event_type="act",
+        manage_event="create",
+        increment_partition=True,
+        log_vars_input={},
+        log_vars_output={"react_step"},
+        verbose=True,
+    )
     def act_sync(self):
         system_message, user_message = self._prepare_messages()
         try:
             react_step = self.lm.sync_respond(
-                system_prompt=system_message,
-                user_prompt=user_message,
-                response_model=ReactResponse,
+                system_message=system_message,
+                user_message=user_message,
+                response_model=ReAct,
                 multi_threaded=self.multi_threaded,
             )
-            
-            print(react_step.action)
-            print(react_step.reasoning)
-            if "failed" in react_step.reasoning.lower():
-                print("User message: ", user_message)
         except Exception as e:
             print(f"Error: {e}")
             raise e
-        return self._process_response(react_step, system_message, user_message)
+        result = self._process_response(react_step, system_message, user_message)
+        return result
 
     def add_observation(self, obs: Dict):
         self.obs_history.append(obs)
+        if self.history.steps:
+            self.history.steps[-1].following_observation = obs
+
+    async def act(self):
+        return await self.act_async()
